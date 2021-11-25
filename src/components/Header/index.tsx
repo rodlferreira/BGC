6d3d6eeb80f0835a70c17cb1aefcbfd114c3ac6a
05206086401 <rodrigo.ferreira@sevensys.tech>
2021-11-25 10:27:57 -0300

    integraçao com  o Axios concluida

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,30 +2,36 @@
 import axios from "axios";
 import React, { useEffect, useState, useCallback } from "react";
 
+interface IProps {
+    name: string;
+    url: string;
+}
+
 
 export default function Header() {
     const [search, setSearch] = useState([]);
-    const [ability, setAbility] = useState({});
+    const [ability, setAbility] = useState<IProps[]>([]);
 
-    useEffect(() => {fetchCard()},[]);
+    useEffect(() => { fetchCard() }, []);
 
-function fetchCard(){
-   axios.get('https://pokeapi.co/api/v2/ability/').then(response => {
-       setAbility(response);
-      console.log("response => ", ability)
-    }
-    ).catch(err => console.log(err))
-}
-        return (
+    function fetchCard() {
+        axios.get('https://pokeapi.co/api/v2/ability/').then(response => {
+       setAbility(response.data.results);
+        }
+    ).catch (err => console.log(err))
+        }
+return (
 
-        <div className="header">
-            <h1>Desafio BGC</h1>
-            <input type="text" />
-            <div>
-                <ul>
-
-                </ul>
-            </div>
+    <div className="header">
+        <h1>Desafio BGC</h1>
+        <input type="text" />
+        <div>
+            <ul>
+                {ability.map((item: IProps) => (
+                    <h2 key={item.name}>{item.name}</h2>
+                    ))}
+            </ul>
         </div>
-    );
+    </div>
+);
 }
